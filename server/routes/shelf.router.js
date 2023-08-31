5ba4229c7ba950a1e4d4f3aa86782e406ebf6bae
Sam Johnson <s_johnso@yahoo.com>
2023-08-31 13:53:36 -0500

    added POST route in api/shelf

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -21,8 +21,19 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
-  // endpoint functionality
+router.post('/', rejectUnauthenticated, (req, res) => {
+  console.log('/api/shelf POST route');
+  console.log(req.body);
+  console.log('user', req.user);
+
+  const queryValues = [req.body.description, req.body.image_url, req.user.id]
+  let queryText = 'INSERT INTO "item" ("description", "image_url", "user_id") VALUES ($1, $2, $3);'
+  pool.query(queryText, queryValues).then((result) => {
+      res.sendStatus(200);
+  }).catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+  });
 });
 
 /**
